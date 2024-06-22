dda2e4927636de13d2f05a90f59b5c647ac29650
sonu-lodhi <sonulodhi.scsit@gmail.com>
2024-06-22 19:51:16 +0530

    DAY-1

--- src/main/java/codewithsonu/dsa/basic/SinglyLinkedList.java
+++ src/main/java/codewithsonu/dsa/basic/SinglyLinkedList.java
@@ -2,4 +2,38 @@ package codewithsonu.dsa.basic;
 
 public class SinglyLinkedList {
 
+    private ListNode head;
+
+    private static class ListNode{
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public void display(){
+        ListNode currentNode = head;
+        while(currentNode!=null){
+            System.out.print(currentNode.data + " --> ");
+            currentNode = currentNode.next;
+        }
+        System.out.print("null");
+    }
+    public static void main(String[] args) {
+        SinglyLinkedList sll = new SinglyLinkedList();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(1);
+        ListNode third = new ListNode(8);
+        ListNode forth = new ListNode(11);
+
+        sll.head.next = second;
+        second.next = third;
+        third.next = forth;
+
+        sll.display();
+    }
+
+
 }
